faeabd8eb585326d06262787d805613e4fda8360
Manuel Casanova <manueljuliocasanova@gmail.com>
2024-02-08 20:16:34 -0500

    works_url_trans

--- src/app/components/LanguageChanger.js
+++ src/app/components/LanguageChanger.js
@@ -6,19 +6,45 @@ import spFlag from "../../../public/img/spainFlag.png";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
+import i18nConfig from '../../../i18nConfig';
 
 export default function LanguageChanger() {
   const { i18n } = useTranslation();
   const [openedDropDownMenu, setOpenedDropDownMenu] = useState(false);
   const [rotated, setRotated] = useState(false);
+  const router = useRouter();
+  const currentPathname = usePathname()
+  const currentLocale = i18n.language;
 
   const handleLanguageChange = (newLocale) => {
     // Change the language directly
     i18n.changeLanguage(newLocale);
 
-    // Close the dropdown menu
+    // set cookie for next-i18n-router
+    const days = 30;
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    const expires = date.toUTCString();
+    document.cookie = `NEXT_LOCALE=${newLocale};expires=${expires};path=/`;
+
+    // redirect to the new locale path
+    if (
+      currentLocale === i18nConfig.defaultLocale &&
+      !i18nConfig.prefixDefault
+    ) {
+      router.push("/" + newLocale + currentPathname);
+    } else {
+      router.push(
+        currentPathname.replace(`/${currentLocale}`, `/${newLocale}`)
+      );
+    }
     setOpenedDropDownMenu(false);
+    router.refresh();
   };
+  // Close the dropdown menu
+
   return (
     <div
       className="text-gray-800 w-36 cursor-pointer flex flex-col justify-between items-center ml-8"
